e10b42372a8fd9bd585e01157a2e04b77c3d5a42
Gi Deok Jang <ziinxed@gmail.com>
2020-07-07 21:25:51 +0900

    express server with middle ware

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("work");
+import { Options } from "graphql-yoga";
+import app from "./app";
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND: string = "/playground";
+const GTAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND,
+  endpoint: GTAPHQL_ENDPOINT,
+};
+
+const handleAppStart = () => {
+  console.log(`Listening on Port ${PORT}`);
+};
+
+app.start(appOptions, handleAppStart);
