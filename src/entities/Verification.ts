e55d77ab221cddbafee3558ece8515631d852cdc
Gi Deok Jang <ziinxed@gmail.com>
2020-07-20 21:43:22 +0900

    verification key entitiy

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -5,14 +5,17 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  BeforeInsert,
 } from "typeorm";
 import { verificationTarget } from "src/types/types";
 
+const Phone = "PHONE";
+const EMAIL = "EMAIL";
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  @Column({ type: "text", enum: [PHONE, EMAIL] })
   target: verificationTarget;
   @Column({ type: "text" })
   payload: string;
@@ -24,6 +27,17 @@ class Verification extends BaseEntity {
 
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
+
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(
+        Math.random() * 100000
+      ).toString();
+    } else {
+      this.key = Math.random().toString(36).substr(2);
+    }
+  }
 }
 
 export default Verification;
